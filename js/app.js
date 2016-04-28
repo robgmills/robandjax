a634a112c766ef91d475babd6863f0073aaf70a6
Rob Mills <rob.mills@uptake.com>
2016-04-27 21:53:11 -0500

    break callback up into smaller functions

--- js/app.js
+++ js/app.js
@@ -21,66 +21,74 @@ photoMapApp.controller('photomap-controller', ['$scope', '$http', function($scop
         defaultFill: '#FFFFFF'
     };
 
-    $http.get('/js/albums.json').success(function(data) {
+    $scope.zoomIn = function(data) {
+        var circle = d3.select(this);
+        circle
+            //.transition() // transitioning seems to cause flickering of the bubble as it grows
+            //.duration(400)
+            .attr('r', function(datum) {
+                return '25%'; // grow the circle by 25%
+            })
+            .style('fill', function(datum) {
+                return 'url(#album' + datum.id + ')';
+            })
+            .style('fill-opacity', 1);
+        circle.moveToFront();
+    };
 
-        $scope.albums = data;
-        $scope.world.bubbles($scope.albums, $scope.bubbleConfig);
+    $scope.zoomOut = function(data) {
+        var circle = d3.select(this);
+        circle
+            //.transition() // transitioning seems to cause flickering of the bubble as it grows
+            //.duration(400)
+            .attr('r', function(datum) {
+                return datum.radius;
+            })
+            .style('fill', function(datum) {
+                return $scope.fills[datum.fillKey];
+            })
+            .style('fill-opacity', .75);
+    };
 
-        var svg = $scope.world.svg,
-            defs = svg.append("defs");
+    $scope.onClick = function(data) {
+        $window.location.href = data.linkUrl;
+    }
+
+    $scope.createPatterns = function(svg, patternData) {
+        var defs = svg.append("defs");
 
         // Adds all the album cover images to the svg patterns to make
         // them available as fills for the bubbles
-        for( var i=0; i<$scope.albums.length; i++ ) {
-            var album = $scope.albums[i];
+        for( var i=0; i<patternData.length; i++ ) {
+            var album = patternData[i];
             defs.append('pattern')
-                    .attr('id', 'album' + album.id)
-                    .attr('height', '100%')
-                    .attr('width', '100%')
-                    .attr('patternContentUnits', 'objectBoundingBox')
-                    .attr('viewBox', '0 0 1 1')
-                    .attr('preserveAspectRatio', 'xMidYMid slice')
+                .attr('id', 'album' + album.id)
+                .attr('height', '100%')
+                .attr('width', '100%')
+                .attr('patternContentUnits', 'objectBoundingBox')
+                .attr('viewBox', '0 0 1 1')
+                .attr('preserveAspectRatio', 'xMidYMid slice')
                 .append('pattern:image')
-                    .attr('xmlns:xlink','http://www.w3.org/1999/xlink')
-                    .attr('xlink:href', album.imageUrl)
-                    .attr('preserveAspectRatio', 'xMidYMid slice')
-                    .attr('height', 1)
-                    .attr('width', 1);
+                .attr('xmlns:xlink','http://www.w3.org/1999/xlink')
+                .attr('xlink:href', album.imageUrl)
+                .attr('preserveAspectRatio', 'xMidYMid slice')
+                .attr('height', 1)
+                .attr('width', 1);
         }
+    }
 
+    $http.get('/js/albums.json').success(function(data) {
+        var svg = $scope.world.svg;
+
+        $scope.albums = data;
+        $scope.world.bubbles($scope.albums, $scope.bubbleConfig);
+        $scope.createPatterns(svg, $scope.albums);
 
         var circles = svg.selectAll("circle");
         circles
-            .on('mouseover.zoom', function(data) {
-                var circle = d3.select(this);
-                circle
-                    //.transition() // transitionining seems to cause flickering of the bubble as it grows
-                    //.duration(400)
-                    .attr('r', function(datum) {
-                        return '25%'; // grow the circle by 25%
-                    })
-                    .style('fill', function(datum) {
-                        return 'url(#album' + datum.id + ')';
-                    })
-                    .style('fill-opacity', 1);
-                circle.moveToFront();
-            })
-            .on('mouseout.zoom', function(data) {
-                var circle = d3.select(this);
-                circle
-                    //.transition() // transitionining seems to cause flickering of the bubble as it grows
-                    //.duration(400)
-                    .attr('r', function(datum) {
-                        return datum.radius;
-                    })
-                    .style('fill', function(datum) {
-                        return $scope.fills[datum.fillKey];
-                    })
-                    .style('fill-opacity', .75);
-            })
-            .on('click', function(data){
-                window.location.href = data.linkUrl;
-            });
+            .on('mouseover.zoom', $scope.zoomIn)
+            .on('mouseout.zoom', $scope.zoomOut)
+            .on('click', $scope.onClick);
 
     });
 
